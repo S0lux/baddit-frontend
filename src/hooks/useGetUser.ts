94c3b766c7280b7e072093aeada6dfffa7e54ee0
Tkhangds <87261264+Tkhangds@users.noreply.github.com>
2024-05-05 15:24:22 +0700

    Minor change

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function useGetUser() {
-  const [logged, setLogged] = useState<string>();
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [userData, setUserData] = useState<any>(null);
 
   const GetUser = async () => {
@@ -10,12 +10,12 @@ export default function useGetUser() {
       const response = await axios.get("https://api.baddit.life/v1/users/me", {
         withCredentials: true,
       });
-      setLogged("logged in");
+      setLoggedIn(true);
       setUserData(response.data);
     } catch (err: any) {
-      setLogged("non-logged in");
+      setLoggedIn(false);
     }
   };
 
-  return { logged, userData, GetUser };
+  return { loggedIn, userData, GetUser };
 }
